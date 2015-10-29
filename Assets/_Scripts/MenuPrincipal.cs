78202f7ef94530906861be3cf3add95843ea942b
lc-luis <luisfernandezmolina@icloud.com>
2015-10-29 02:14:22 +0100

    MenuPrincipal

---
+++ Assets/_Scripts/MenuPrincipal.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MenuPrincipal : MonoBehaviour 
+{
+
+	public Button boton;
+
+	// Use this for initialization
+	void Start () 
+	{
+		boton = boton.GetComponent<Button> ();
+	}
+	
+	// Llama al Level 1
+	public void IniciarAplicacion () 
+	{
+		Application.LoadLevel (1);
+	}
+}
