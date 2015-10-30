e80112c99ca79edbd87af6944698ce10c8c43225
lc-luis <luisfernandezmolina@icloud.com>
2015-10-30 01:15:12 +0100

    Creacion Objetos
    -Chapa, Terreno, y Proyecto Touchj

---
+++ Assets/_Scripts/Fisica_Chapa.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fisica_Chapa : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb.AddForce (Vector3.right * 50f, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- Assets/_Scripts/MenuPrincipal.cs
+++ Assets/_Scripts/MenuPrincipal.cs
@@ -6,16 +6,23 @@ public class MenuPrincipal : MonoBehaviour
 {
 
 	public Button boton;
+	public Button botonDemo;
 
 	// Use this for initialization
 	void Start () 
 	{
 		boton = boton.GetComponent<Button> ();
+		botonDemo = botonDemo.GetComponent<Button> ();
 	}
 	
 	// Llama al Level 1
 	public void IniciarAplicacion () 
 	{
 		Application.LoadLevel (1);
+	}
+	// Llama al Level DEMO
+	public void IniciarDemo () 
+	{
+		Application.LoadLevel (2);
 	}
 }
