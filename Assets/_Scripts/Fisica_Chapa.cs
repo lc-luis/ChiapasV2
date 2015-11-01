9473f1dde60fe0ecb3d7139fa84c5e9916f73872
lc-luis <luisfernandezmolina@icloud.com>
2015-11-01 23:08:11 +0100

    Pruebas de Fisica en Script

--- Assets/_Scripts/Fisica_Chapa.cs
+++ Assets/_Scripts/Fisica_Chapa.cs
@@ -3,15 +3,25 @@ using System.Collections;
 
 public class Fisica_Chapa : MonoBehaviour {
 
-	public Rigidbody rb;
+	//public Rigidbody rb;
+	public int velocidad = 10;
 
 	// Use this for initialization
 	void Start () {
-		rb.AddForce (Vector3.right * 50f, ForceMode.Impulse);
+		//rb.AddForce (Vector3.right * 50f, ForceMode.Impulse);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 	}
+
+	//Fuerzas continua hacia arriba, tiene en cuenta gravedad y masa del objeto
+	void FixedUpdate ()
+	{
+		//GetComponent<Rigidbody> ().AddForce (new Vector3 (0, velocidad, 0), ForceMode.Force);
+		//GetComponent<Rigidbody> ().AddTorque (Vector3.up, ForceMode.Impulse);
+	}
+
+
 }
